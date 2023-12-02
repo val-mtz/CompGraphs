80d79ced0a06a42a4303819564a87cfd3f720fc9
val-mtz <A01782413@tec.mx>
2023-12-01 22:56:25 -0600

    bruh

--- 3DTest/Assets/Scripts/CarMovement.cs
+++ 3DTest/Assets/Scripts/CarMovement.cs
@@ -1,55 +1,66 @@
-// Desplazar coche hacia una dirección dada mientras se desplazan y giran las ruedas
-// Valeria Martínez Martínez - A01782413
-// 2023/12/01
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CarAttempt : MonoBehaviour
+public class CarMovement : MonoBehaviour
 {
     [SerializeField] Vector3 displacement;
-    [SerializeField] float rotationAngle;
+    [SerializeField] float angleW;
     float angle;
-    public GameObject wheelPrefab;
+    public GameObject wheel;
 
+    // car mesh
     Mesh mesh;
 
+    // array for vertices and base vertices of the car
     Vector3[] newVertices;
     Vector3[] baseVertices;
 
+    // wheel meshes
     Mesh[] wheelMeshes = new Mesh[4];
 
+    // wheel vertices
     Vector3[][] wheelBaseVertices = new Vector3[4][];
     Vector3[][] wheelNewVertices = new Vector3[4][];
     
+    // wheel game objects
     GameObject[] wheels = new GameObject[4];
 
+    // wheel positions
     [SerializeField] Vector3[] wheelPositions;
 
+    // Start is called before the first frame update
     void Start()
     {
+        // instantiate wheels in origin 
         for(int i = 0; i < wheels.Length; i++)
         {
-            wheels[i] = Instantiate(wheelPrefab, Vector3.zero, Quaternion.identity);
+            wheels[i] = Instantiate(wheel, Vector3.zero, Quaternion.identity);
+
         }
 
+        // get mesh of the car
         mesh = GetComponentInChildren<MeshFilter>().mesh;
 
+        // base Vertices of the car
         baseVertices = mesh.vertices;
 
+        // copy the vertices of the car
         newVertices = new Vector3[baseVertices.Length];
         for (int i = 0; i < baseVertices.Length; i++)
         {
             newVertices[i] = baseVertices[i];
+
         }
 
+        // Get meshes and vertices for the wheels
         for (int i = 0; i < 4; i++)
         {
             wheelMeshes[i] = wheels[i].GetComponentInChildren<MeshFilter>().mesh;
             wheelBaseVertices[i] = wheelMeshes[i].vertices;
         }
 
+        // copy vertices of the wheels
         for (int i = 0; i < 4; i++)
         {
             wheelNewVertices[i] = new Vector3[wheelBaseVertices[i].Length];
@@ -58,8 +69,10 @@ public class CarAttempt : MonoBehaviour
                 wheelNewVertices[i][j] = wheelBaseVertices[i][j];
             }
         }
+
     }
 
+    // Update is called once per frame
     void Update()
     {
         DoTransform();
@@ -69,28 +82,28 @@ public class CarAttempt : MonoBehaviour
     {
         angle = Mathf.Atan2(displacement.x, displacement.z) * Mathf.Rad2Deg;
 
-        Matrix4x4 translation = HW_Transforms.TranslationMat(displacement.x * Time.time, displacement.y * Time.time, displacement.z * Time.time);
+        Matrix4x4 move = HW_Transforms.TranslationMat(displacement.x * Time.time, displacement.y * Time.time, displacement.z * Time.time);
         Matrix4x4 moveOrigin = HW_Transforms.TranslationMat(-displacement.x, -displacement.y, -displacement.z);
         Matrix4x4 moveObject = HW_Transforms.TranslationMat(displacement.x, displacement.y, displacement.z);
         Matrix4x4 rotate = HW_Transforms.RotateMat(angle, AXIS.Y);
 
-        Matrix4x4 composite = translation * rotate;
+        Matrix4x4 carComposite = move * rotate;
 
         for (int i = 0; i < newVertices.Length; i++)
         {
             Vector4 temp = new Vector4(baseVertices[i].x, baseVertices[i].y, baseVertices[i].z, 1);
-            newVertices[i] = composite * temp;
+            newVertices[i] = carComposite * temp;
         }
 
         mesh.vertices = newVertices;
         mesh.RecalculateNormals();
 
-        Matrix4x4 rotateWheel = HW_Transforms.RotateMat(rotationAngle, AXIS.X);
+        Matrix4x4 rotateW = HW_Transforms.RotateMat(angleW * Time.time, AXIS.X);
 
         for (int i = 0; i < 4; i++)
         {
-            Matrix4x4 wheelTranslation = HW_Transforms.TranslationMat(wheelPositions[i].x, wheelPositions[i].y, wheelPositions[i].z);
-            Matrix4x4 wheelComposite = composite * wheelTranslation * rotateWheel;
+            Matrix4x4 wheelTransform = HW_Transforms.TranslationMat(wheelPositions[i].x, wheelPositions[i].y, wheelPositions[i].z);
+            Matrix4x4 wheelComposite = carComposite * wheelTransform * rotateW;
 
             for (int j = 0; j < wheelNewVertices[i].Length; j++)
             {
